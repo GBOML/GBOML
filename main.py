a3ac08bbb2861a4d1cc019b129354fe2d7e9245c
Mathias Berger <>
2021-01-25 10:07:41 +0100

    Minor fixes to main

--- main.py
+++ main.py
@@ -15,16 +15,16 @@ import matplotlib.pyplot as plt
 import matplotlib
 import numpy as np
 import sys
-from julia import Main 
 #from julia.api import Julia
 #jpath = "/Applications/Julia-1.5.app/Contents/Resources/julia/bin/julia"
 #jl = Julia(runtime=jpath,compiled_modules=False)
+from julia import Main
 import pandas as pd
 import os
 import json
 import gurobipy as grbp
 from gurobipy import GRB
-#import cplex
+import cplex
 
 def solver_gurobi(A, b, c):
     A = A.astype(float)
@@ -36,9 +36,9 @@ def solver_gurobi(A, b, c):
     x = model.addMVar(shape=n, lb=-float('inf'), ub=float('inf'), vtype=GRB.CONTINUOUS, name="x")
     model.addMConstr(A, x, '<', b)
     model.setObjective(c @ x, GRB.MINIMIZE)
-    model.setParam('Method',2)
-    model.setParam('BarHomogeneous',1)
-    model.setParam('Crossover',0)
+    model.setParam('Method',2)          # uses a barrier method
+    model.setParam('BarHomogeneous',1)  # uses a barrier variant with better numerical stability
+    model.setParam('Crossover',0)       # disables crossover (returns a nonbasic solution)
 
     try:
         model.optimize()
@@ -59,13 +59,13 @@ def solver_cplex(A, b, c):
     c = c.tolist()[0]
 
     model = cplex.Cplex()
-    model.variables.add(obj=c,lb=[-cplex.infinity]*n,ub=[cplex.infinity]*n,types=['C']*n)
+    model.variables.add(obj=c,lb=[-cplex.infinity]*n,ub=[cplex.infinity]*n)
     model.linear_constraints.add(senses=['L']*m,rhs=b)
     model.linear_constraints.set_coefficients(A_zipped)
     model.objective.set_sense(model.objective.sense.minimize)
     alg = model.parameters.lpmethod.values
-    model.parameters.lpmethod.set(alg.barrier)
-    model.parameters.barrier.crossover.set(model.parameters.barrier.crossover.values.none)
+    model.parameters.lpmethod.set(alg.barrier)    # uses a barrier method
+    model.parameters.barrier.crossover.set(model.parameters.barrier.crossover.values.none)    # disables crossover (yields a nonbasic solution)
 
     try:
         model.solve()
@@ -99,7 +99,7 @@ def solver_julia_2(A,b,C):
     x = None
 
     Main.include("linear_solver.jl") # load the MyFuncs module
-    try : 
+    try :
         x, optimal ,status= Main.lin_solve_sparse(C.astype(float),constraint_matrix.astype(float),b.astype(float))
     except RuntimeError as e:
         print(e)
@@ -114,15 +114,15 @@ def solver_julia(A,b,C):
     flag_solved = False
     x = None
     Main.include("linear_solver.jl") # load the MyFuncs module
-    try : 
+    try :
         x = Main.lin_solve(C.astype(float),A.astype(float),b.astype(float))
         flag_solved = True
-    except(RuntimeError): 
+    except(RuntimeError):
         flag_solved = False
     return x,flag_solved
 
 def plot_results(x,T,name_tuples):
-    
+
     legend = []
     font = {'size'   : 15}
 
@@ -168,7 +168,7 @@ def convert_pandas(x,T,name_tuples):
     for node_name,index_variables in name_tuples:
         for index,variable in index_variables:
             full_name = str(node_name)+"."+str(variable)
-            values = x[index:(index+T)].flatten() 
+            values = x[index:(index+T)].flatten()
             columns.append(full_name)
             ordered_values.append(values)
 
@@ -192,11 +192,11 @@ def compile_file(directory,file,log=False):
     x,_ = solver_julia_2(A,b,C_sum)
     T = program.get_time().get_value()
     panda_datastruct = convert_pandas(x,T,name_tuples)
-    
+
     filename_split = file.split(".")
     filename = filename_split[0]
 
-    
+
     panda_datastruct.to_csv(filename+".csv")
 
 
@@ -209,7 +209,7 @@ if __name__ == '__main__':
     parser.add_argument("--lex",help="Prints all tokens found in input file",action='store_const',const=True)
     parser.add_argument("--parse",help="Prints the AST",action='store_const',const=True)
     parser.add_argument("--matrix",help="Prints matrix representation",action='store_const',const=True)
-    
+
     parser.add_argument("--json", help="Convert results to JSON format",action='store_const',const=True)
     parser.add_argument("--csv", help="Convert results to CSV format",action='store_const',const=True)
 
@@ -223,7 +223,7 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    if args.linprog==False: 
+    if args.linprog==False:
         print("The default solver is GUROBI")
 
     if args.input_file:
@@ -240,13 +240,13 @@ if __name__ == '__main__':
         curr_dir = os.getcwd()
 
         dir_path = os.path.dirname(args.input_file)
-        filename = os.path.basename(args.input_file) 
+        filename = os.path.basename(args.input_file)
         #print(dir_path)
         os.chdir(dir_path)
 
         if args.lex:
             tokenize_file(filename)
-        
+
 
         result = parse_file(filename)
 
@@ -255,7 +255,7 @@ if __name__ == '__main__':
         start_time = time.time()
 
         program = semantic(result)
-        
+
         T = program.get_time().get_value()
 
         A,b,name_tuples = matrix_generationAb(program)
@@ -288,11 +288,11 @@ if __name__ == '__main__':
             x, status = solver_cplex(A,b,C_sum)
         elif args.gurobi:
             x, status = solver_gurobi(A,b,C_sum)
-        else: 
+        else:
             print("No solver was chosen")
             exit()
 
-        if status == False : 
+        if status == False :
             print("An error occured !")
             exit()
         elif status == "no solution":
@@ -300,11 +300,11 @@ if __name__ == '__main__':
             exit()
 
         #plot_results(x,T,name_tuples)
-        
+
         filename_split = args.input_file.rsplit('.', 1)
         filename = filename_split[0]
 
-        if args.json: 
+        if args.json:
             dictionary = convert_dictionary(x,T,name_tuples,optimal,status,program.to_dict())
             with open(filename+".json", 'w') as outfile:
                 json.dump(dictionary, outfile,indent=4)
@@ -312,6 +312,6 @@ if __name__ == '__main__':
             panda_datastruct = convert_pandas(x,T,name_tuples)
             panda_datastruct.to_csv(filename+".csv")
 
-    else: 
+    else:
         print('ERROR : expected input file')
     print("--- %s seconds ---" % (time.time() - start_time))
