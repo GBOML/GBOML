5336fe0234bd760ae403ba7ce791754a7aa6d1a9
Derval Guillaume <gderval@uliege.be>
2024-06-25 11:54:51 +0200

    Fix removed function

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from lark.exceptions import UnexpectedCharacters, UnexpectedToken
 
 from gboml.ast.check import check
-from gboml.parsing import parse_file
+from gboml.parsing import GBOMLParser
 
 instance_dir = Path(__file__).parent / "instances"
 
@@ -22,19 +22,21 @@ should_not_pass = [x
                    if str(x).endswith(".txt")]
 
 
+parser = GBOMLParser()
+
 class TestParsing(unittest.TestCase):
     def test_parse(self):
         """ Checks if the file (doesn't) parses """
         for filename in should_pass:
             with self.subTest(filename=filename):
-                out = parse_file(str(filename))
+                out = parser.parse_file(str(filename))
                 check(out)
 
     def test_not_parse(self):
         for filename in should_not_pass:
             with self.subTest(filename=filename):
                 try:
-                    out = parse_file(str(filename))
+                    out = parser.parse_file(str(filename))
                     check(out)
                 except:
                     continue
