fd8077ace0f472979a0add150709ea068aec4eac
T-3B <cle.smagghe@gmail.com>
2024-06-26 12:46:21 +0200

    globalScope checking - not rootScope anymore

--- src/main.py
+++ src/main.py
@@ -4,62 +4,71 @@ from gboml.parsing import GBOMLParser
 from gboml.semantic import semantic_check
 from gboml.scope import RootScope
 
-tree = GBOMLParser().parse("""// Test working constrainting and optimizing only one element of vector
-
-#TIMEHORIZON
-T = 10;
+tree = GBOMLParser().parse("""
+#TIMEHORIZON T = 1;
+#GLOBAL
+    b = 4;
 
 #NODE A
-#VARIABLES 
-internal : x[T];
-#CONSTRAINTS
-x[T-1]>=0;
-#OBJECTIVES
-min: x[T-1];
+    #PARAMETERS
+        a = 1;
+    #NODE B
+        #PARAMETERS
+            b = 2;
+        #NODE C
+            #PARAMETERS
+                c = 3;
+            #NODE D
+                #PARAMETERS
+                    d = 4;
+                #VARIABLES
+                    internal : x;
+                #CONSTRAINTS
+                    x >= A.a;
+            #VARIABLES
+                internal : x <- D.x;
+            #CONSTRAINTS
+                x <= B.b+A.a+c+global.b;
+        #VARIABLES
+            internal : x <- C.x;
+    #VARIABLES
+        internal : x <- B.x;
+    #OBJECTIVES
+        min : x + a;
 """)
-    # c = 2e2;
-    # d = -1;
-    # e = 2e-2;
-# #NODE nodeI  = import nodeA from "hello.gboml";
-# #NODE nodeI2 = import nodeA from "hello.gboml" with
-    # a = 2;
-    # b external;
-    # c internal;
-# #NODE node1
-    # #PARAMETERS
-        # a = 2;
-        # b = import "lol.csv";
-    # #NODE A
-        # #VARIABLES
-            # external: x;
-            # external: y;
-    # #HYPEREDGE E
-        # #PARAMETERS
-            # a = 2;
-        # #CONSTRAINTS
-            # A.x == A.y;
-    # #VARIABLES
-        # internal: a;
-        # external: b;
-        # internal continuous: c[T];
-        # internal integer: d[T+2];
-        # internal binary: e[T-2];
-    # #CONSTRAINTS
-        # 2+f(2,2,{2})*(2-2/2%2**2) == -2.2*0;
-        # 2 >= 0;
-        # 0 <= 2;
-        # a == b;
-        # named: a == b for i in [0:2] where ((i % 2) == 0) and i > 0 or (i < 0 or i != 0 and not i == i and i <= 0 and i >= 0);
-        # SOS1 {a, b};
-        # SOS2 {a, b};
-        # sum(a*2 for a in [0:2]) == 2;
-    # #OBJECTIVES
-        # min named: a;
-        # max: b[i] for i in [0:2*4] where i % 2 == 1;
-# #HYPEREDGE he1 = import lal from "x" with
-    # a = 2;
-    # b = 2;
-# """)
+
+
+['B', 'b']
+['A', 'a']
+['c']
+['global', 'b']
+['x']
+['A', 'a']
+['x']
+['D', 'x']
+['C', 'x']
+['x']
+['a']
+['B', 'x']
+['B', 'b']
+['A', 'a']
+['c']
+['global', 'b']
+['x']
+['A', 'a']
+['x']
+['D', 'x']
+['C', 'x']
+['B', 'b']
+['A', 'a']
+['c']
+['global', 'b']
+['x']
+['A', 'a']
+['x']
+['D', 'x']
+['A', 'a']
+['x']
 
 print(tree)
 # print(tree.meta)
