7a23e9a2ad6ae2babf9ebb2a0bb5d667c6e3a2ab
T-3B <cle.smagghe@gmail.com>
2024-07-05 09:30:37 +0200

    allow fct to take arrays/dicts as args

--- src/gboml/semantic.py
+++ src/gboml/semantic.py
@@ -41,10 +41,12 @@ def _check_var_in_scope(element: VarOrParam, hier: list[NodeDefinition|NodeGener
             else:
                 raise KeyError(f"SEMANTIC ERROR: {leaf.name} (from {list(map(lambda e: e.name, element.path))}) can not be used in this scope {leaf.meta}!")
 
+        if isinstance(parentScope, HasLoopInScope | ScopedFunctionDefinition):
+            break
         isUsedAsArray = bool(leaf.indices)
         if isUsedAsArray != isDeclaredAsArray:
             raise KeyError(f"SEMANTIC ERROR: {leaf.name} (from {list(map(lambda e: e.name, element.path))}): mixing declaration type and use type (array Vs. scalar) {leaf.meta}!")
-        if isDeclaredAsArray or isinstance(parentScope, HasLoopInScope | ScopedFunctionDefinition):
+        if isDeclaredAsArray:
             break
 
     # visit all VarOrParam indices at once
