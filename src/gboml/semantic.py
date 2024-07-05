8185e2a4897dd7d842fd2f590da380dfa02c4534
T-3B <cle.smagghe@gmail.com>
2024-07-05 11:06:51 +0200

    support foreach loop

--- src/gboml/semantic.py
+++ src/gboml/semantic.py
@@ -24,33 +24,40 @@ def _check_var_in_scope(element: VarOrParam, hier: list[NodeDefinition|NodeGener
     # if there is any parent VarOrParam in hier, return (sub-VarOrParam are handled from the parent)
     if any(isinstance(hierItem, VariableDefinition | VarOrParam) for hierItem in reversed(hier[:-1])):
         return
-    # print(element, list(map(lambda _: (type(_), isinstance(_, NodeDefinition | HyperEdgeDefinition| FunctionDefinition | GeneratedRValue)), hier)))
+    # print()
+    # boo = element.path[0].name == 'x' and len(element.path) == 1
+    # if boo:
+        # print(element, list(map(lambda _: (type(_), isinstance(_, NodeDefinition | HyperEdgeDefinition| FunctionDefinition | GeneratedRValue)), hier)))
     if scope is None:  # get the scope of the last node/fct/genrval in hier
         scope = next(hierItem.scope for hierItem in reversed(hier) if isinstance(hierItem, (*HasLoopInScope.astTypes, NodeDefinition, HyperEdgeDefinition, FunctionDefinition)))
-    parentScope = scope
+    origScope = scope
     for leaf in element.path[:2]:
-        # print(leaf.name, type(scope), scope.content.keys())
+        # if boo:
+            # print(leaf.name, type(scope), scope.content.keys())
         try:
             scope = scope[leaf.name]
             isDeclaredAsArray = isinstance(scope, ScopedVariableDefinition) and bool(scope.ast.indices) or isinstance(scope, ScopedDefinition) and isinstance(scope.ast.value, Array)
         except KeyError:
             # if TIMEHORIZON is set, 'T' and 't' are allowed
-            if scope and (leaf.name == 't' or leaf.name == 'T') and parentScope['global'].parent.ast.time_horizon is not None:
+            if scope and (leaf.name == 't' or leaf.name == 'T') and origScope['global'].parent.ast.time_horizon is not None:
                 isDeclaredAsArray = False  # indices are not allowed
                 scope = {}  # a following leaf in element.path is not allowed (next leaf.name will raise KeyError)
             else:
                 raise KeyError(f"SEMANTIC ERROR: {leaf.name} (from {list(map(lambda e: e.name, element.path))}) can not be used in this scope {leaf.meta}!")
 
-        if isinstance(parentScope, HasLoopInScope | ScopedFunctionDefinition):
+        # if boo and origScope.ast.loop is not None:
+            # print()
+        if isinstance(origScope, HasLoopInScope | ScopedFunctionDefinition) and not scope:
             break
-        isUsedAsArray = bool(leaf.indices)
-        if isUsedAsArray != isDeclaredAsArray:
+        isBeingIteratedOn = leaf is element.path[-1] and isinstance(origScope, HasLoopInScope) and origScope.ast.loop is not None and element is origScope.ast.loop.on
+        isUsedAsArray = bool(leaf.indices) or isBeingIteratedOn
+        if isUsedAsArray != isDeclaredAsArray: # and not (isinstance(origScope, HasLoopInScope) and origScope.ast.loop is not None):
             raise KeyError(f"SEMANTIC ERROR: {leaf.name} (from {list(map(lambda e: e.name, element.path))}): mixing declaration type and use type (array Vs. scalar) {leaf.meta}!")
         if isDeclaredAsArray:
             break
 
     # visit all VarOrParam indices at once
-    visit(element, {VarOrParam: lambda var: None if var is element else _check_var_in_scope(var, scope = parentScope)})
+    visit(element, {VarOrParam: lambda var: None if var is element else _check_var_in_scope(var, scope = origScope)})
 
 def semantic_check(globalScope: GlobalScope):
     # check if variables are in scope
