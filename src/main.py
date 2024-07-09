516a8ba981294f5860f849734459eece9fa0c806
T-3B <cle.smagghe@gmail.com>
2024-07-09 14:16:51 +0200

    check for array misuse in expression op

--- src/main.py
+++ src/main.py
@@ -20,8 +20,10 @@ def _extend_multiloop(mloop: MultiLoop) -> BaseLoop:
     return prevloop
 
 
+# FunctionDefinition(name='f', args=['b'], value=ExpressionOp(operator=<Operator.exponent: '**'>, operands=[VarOrParam(path=[VarOrParamLeaf(name='global', indices=[]), VarOrParamLeaf(name='pi', indices=[])]), VarOrParam(path=[VarOrParamLeaf(name='b', indices=[VarOrParam(path=[VarOrParamLeaf(name='b', indices=[])])]), VarOrParamLeaf(name='x', indices=[])])]), tags=set()), ConstantDefinition(name='dict', value=Dictionary(content=[DictEntry(key=ExpressionOp(operator=<Operator.minus: '-'>, operands=[ExpressionOp(operator=<Operator.times: '*'>, operands=[Function(name='f', operands=[VarOrParam(path=[VarOrParamLeaf(name='param', indices=[])])]), VarOrParam(path=[VarOrParamLeaf(name='w', indices=[])])]), 3]), value=VarOrParam(path=[VarOrParamLeaf(name='P', indices=[])]), loop=BaseLoop(varid='w', on=Range(start=1, end=3, step=2), condition=None, loop=None)), DictEntry(key='je', value=VarOrParam(path=[VarOrParamLeaf(name='B', indices=[])]), loop=None)]), tags=set()), ConstantDefinition(name='hello', value=Range(start=0, end=2, step=None), tags=set())], nodes=[NodeDefinition(name='P', import_from=None, parameters=[], nodes=[], hyperedges=[], variables=[], constraints=[], objectives=[], activations=[], tags=set()), NodeGenerator(name='GEN', indices=['i', 'j'], loop=BaseLoop(varid='i', on=Range(start=0, end=3, step=None), condition=BoolExpressionComparison(lhs=VarOrParam(path=[VarOrParamLeaf(name='i', indices=[])]), operator=<Operator.equal: '=='>, rhs=3), loop=BaseLoop(varid='j', on=Range(start=3, end=6, step=None), condition=None, loop=None)), import_from=None, parameters=[ConstantDefinition(name='x', value=ExpressionOp(operator=<Operator.times: '*'>, operands=[VarOrParam(path=[VarOrParamLeaf(name='i', indices=[])]), VarOrParam(path=[VarOrParamLeaf(name='j', indices=[])])]), tags=set())], nodes=[], hyperedges=[], variables=[], constraints=[], objectives=[], activations=[], tags=set()), NodeDefinition(name='B', import_from=None, parameters=[ConstantDefinition(name='param', value=2, tags=set())], nodes=[NodeDefinition(name='C', import_from=None, parameters=[ConstantDefinition(name='param', value=3, tags=set())], nodes=[NodeDefinition(name='D', import_from=None, parameters=[ConstantDefinition(name='param', value=4, tags=set())], nodes=[], hyperedges=[], variables=[VariableDefinition(name='x', indices=[VarOrParam(path=[VarOrParamLeaf(name='T', indices=[])])], scope=<VarScope.external: 'external'>, type=<VarType.continuous: 'continuous'>, bound_lower=None, bound_upper=None, import_from=None, tags=set())], constraints=[StdConstraint(name=None, lhs=VarOrParam(path=[VarOrParamLeaf(name='x', indices=[VarOrParam(path=[VarOrParamLeaf(name='t', indices=[])])])]), op=<Operator.greater_or_equal: '>='>, rhs=VarOrParam(path=[VarOrParamLeaf(name='A', indices=[]), VarOrParamLeaf(name='param', indices=[])]), loop=None, tags=set())], objectives=[], activations=[], tags=set()), NodeDefinition(name='E', import_from=None, parameters=[ConstantDefinition(name='param', value=5.5, tags=set())], nodes=[], hyperedges=[], variables=[VariableDefinition(name='y', indices=[VarOrParam(path=[VarOrParamLeaf(name='T', indices=[])])], scope=<VarScope.external: 'external'>, type=<VarType.integer: 'integer'>, bound_lower=None, bound_upper=None, import_from
+
 tree = GBOMLParser().parse("""
-#TIMEHORIZON T = 24;
+#TIMEHORIZON T = 2*2;
 #GLOBAL
     a = 75;
     pi = 314;
@@ -91,7 +93,7 @@ tree = GBOMLParser().parse("""
     #VARIABLES
         internal : x[T] <- B.x[T];
     #OBJECTIVES
-        min : x[t-5] + sum(l for l in hello where l < 2) + f(global.pi) + subnodes[param];
+        min : x[t-5] + sum(l for l in hello where l < 2) + len(hello) + f(global.pi) + subnodes[param];
 
 """)
 
