aecc9391975ee2bf77cef3da29d368d8b1ab5493
T-3B <cle.smagghe@gmail.com>
2024-09-11 23:55:31 +0200

    allow function to be value of DictEntry

--- src/gboml/semantic.py
+++ src/gboml/semantic.py
@@ -33,9 +33,12 @@ def _check_var_or_param_scoping(elem: ExpressionDotCall|PathRoot, hier: list[Hie
     if isinstance(hier[-2], ExpressionFunctionCall|ExpressionDotCall) and hier[-2].lhs is elem:
         return
 
-    print(HierTypes)
     scope = get_scope_after_expr(elem, get_scope_from_hier(hier))
-    if not (isinstance(hier[-2], Array) and elem in hier[-2].content or isinstance(hier[-2], ExpressionFunctionCall) and elem in hier[-2].operands):
+    # a single PathRoot as array element, DictEntry value or ExpressionFunctionCall could be a function_call without argument (even if definition has 1+ arg)
+    if not ((isinstance(hier[-2], Array) and elem in hier[-2].content
+             or isinstance(hier[-2], DictEntry) and elem is hier[-2].value
+             or isinstance(hier[-2], ExpressionFunctionCall) and elem in hier[-2].operands
+            ) and isinstance(elem, PathRoot)):
         _check_fct_use_and_def(elem, scope)
     _add_dep(deps, hier, scope)
 
